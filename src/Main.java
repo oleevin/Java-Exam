178fb2163f6164bc9cdf8f303579edca08bac542
OL EEVIN <138324008+oleevin@users.noreply.github.com>
2024-01-11 19:44:27 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-
+        Scanner inter = new Scanner(System.in);
+        System.out.print("Input Year-Month-Day : ");
+        String shortDate = inter.nextLine();
+        LocalDate date = LocalDate.parse(shortDate);
+        String day = Integer.toString(date.getDayOfMonth());
+        String month = date.getMonth().toString();
+        String year = Integer.toString(date.getYear());
+        System.out.println(month + " " + day + " " + year);
     }
 }
