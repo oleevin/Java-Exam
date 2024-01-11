29dbc2427ef52051484a9cffef3710108a572f26
OL EEVIN <138324008+oleevin@users.noreply.github.com>
2024-01-11 18:31:40 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
 
-        System.out.println("Jkjsdjfhjshdfkjshjkfhsadf");
+
+
     }
 }
