c1ff421771b593419729e5bf6438b7f7438e32a0
OL EEVIN <138324008+oleevin@users.noreply.github.com>
2024-01-11 18:27:17 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-git
         System.out.println("Hello world!");
-
+        System.out.println("Jkjsdjfhjshdfkjshjkfhsadf");
     }
 }
